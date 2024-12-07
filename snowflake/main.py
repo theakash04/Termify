e4e4f1b8c573ebc27a1a64cf9adbc3796eddb4a6
SKY <Aakashkkr04@gmail.com>
2024-12-07 23:59:08 +0530

    Got elightment at 12 pm (#19)

--- snowflake/main.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from sessions import SnowflakeConnector
-
-load_dotenv()
-
-snowflake_connector = SnowflakeConnector()
-snowflake_connector.get_session()
-snowflake_connector.close_connection()
